a62bcdab2a248af56177c636f13db91165a12be5
OmobolajiDurojaiye <durojaiyeomobolaji93@gmail.com>
2025-07-11 18:46:19 +0100

    Se pyton version to 3.11

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from pkg import create_app
 
 app = create_app()
-
 application = app
 
 if __name__ == '__main__':
-    app.config.from_pyfile("config.py")
     app.run(debug=True, host='0.0.0.0', port=5000)
